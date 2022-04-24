590394a10bf8a0f496aea68b912ba63cf383cdbe
chengkai.sheng <chengkai.sheng@zhejintech.com>
2022-04-24 17:56:12 +0800

    包修改

---
+++ datains-extensions-view/view-bubblemap/view-bubblemap-backend/src/main/java/io/datains/plugins/view/official/impl/BubbleMapService.java
@@ -0,0 +1,82 @@
+package io.datains.plugins.view.official.impl;
+
+import io.datains.plugins.common.dto.StaticResource;
+import io.datains.plugins.view.entity.PluginViewField;
+import io.datains.plugins.view.entity.PluginViewParam;
+import io.datains.plugins.view.entity.PluginViewType;
+import io.datains.plugins.view.service.ViewPluginService;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BubbleMapService extends ViewPluginService {
+
+    private static final String VIEW_TYPE_VALUE = "buddle-map";
+    /*下版这些常量移到sdk*/
+    private static final String TYPE = "-type";
+    private static final String DATA = "-data";
+    private static final String STYLE = "-style";
+    private static final String VIEW = "-view";
+    private static final String SUFFIX = "svg";
+    /*下版这些常量移到sdk*/
+    private static final String VIEW_TYPE = VIEW_TYPE_VALUE + TYPE;
+    private static final String VIEW_DATA = VIEW_TYPE_VALUE + DATA;
+    private static final String VIEW_STYLE = VIEW_TYPE_VALUE + STYLE;
+    private static final String VIEW_VIEW = VIEW_TYPE_VALUE + VIEW;
+
+
+    @Override
+    public PluginViewType viewType() {
+        PluginViewType pluginViewType = new PluginViewType();
+        pluginViewType.setRender("echarts");
+        pluginViewType.setCategory("chart.chart_type_space");
+        pluginViewType.setValue(VIEW_TYPE_VALUE);
+        return pluginViewType;
+    }
+
+    @Override
+    public Object format(Object o) {
+        return null;
+    }
+
+    @Override
+    public List<String> components() {
+        List<String> results = new ArrayList<>();
+        results.add(VIEW_VIEW);
+        results.add(VIEW_DATA);
+        results.add(VIEW_TYPE);
+        results.add(VIEW_STYLE);
+        return results;
+    }
+
+    @Override
+    public List<StaticResource> staticResources() {
+        List<StaticResource> results = new ArrayList<>();
+        StaticResource staticResource = new StaticResource();
+        staticResource.setName(VIEW_TYPE_VALUE);
+        staticResource.setSuffix(SUFFIX);
+        results.add(staticResource);
+        return results;
+    }
+
+    @Override
+    protected InputStream readContent(String s) {
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("static/" + s);
+        return resourceAsStream;
+    }
+
+    @Override
+    public String generateSQL(PluginViewParam param) {
+        List<PluginViewField> xAxis = param.getFieldsByType("xAxis");
+        List<PluginViewField> yAxis = param.getFieldsByType("yAxis");
+        if (CollectionUtils.isEmpty(xAxis) || CollectionUtils.isEmpty(yAxis)){
+            return null;
+        }
+        return super.generateSQL(param);
+    }
+
+}
